5b78b485c49fb3ea8e92650b2e0ac6f30e0b7943
bahariandikoo <bahariandikoo@gmail.com>
2021-12-15 13:51:57 +0700

    EAS D4

--- resources/views/keranjang/tambah.blade.php
+++ resources/views/keranjang/tambah.blade.php
@@ -29,37 +29,11 @@
                     <span class="input-group-addon">
                         <span> Rp. </span>
                       </span>
-                    <input type="text" class="form-control" name="harga" id="dengan-rupiah">
+                    <input type="number" class="form-control" name="harga" id="dengan-rupiah">
                 </div>
             </div> <br>
         </div>
         <button type="submit" class="btn btn-primary"> + Tambah Data</button>
 	</form>
 
-    <script type="text/javascript">
-        /* Dengan Rupiah */
-    var dengan_rupiah = document.getElementById('dengan-rupiah');
-    dengan_rupiah.addEventListener('keyup', function(e)
-    {
-        dengan_rupiah.value = formatRupiah(this.value, '');
-    });
-
-    /* Fungsi */
-    function formatRupiah(angka, prefix)
-    {
-        var number_string = angka.replace(/[^,\d]/g, '').toString(),
-            split    = number_string.split(','),
-            sisa     = split[0].length % 3,
-            rupiah     = split[0].substr(0, sisa),
-            ribuan     = split[0].substr(sisa).match(/\d{3}/gi);
-
-        if (ribuan) {
-            separator = sisa ? '.' : '';
-            rupiah += separator + ribuan.join('.');
-        }
-
-        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-        return prefix == undefined ? rupiah : (rupiah ? '' + rupiah : '');
-    }
-    </script>
 @endsection
